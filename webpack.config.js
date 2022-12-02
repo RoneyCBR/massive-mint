21fc7e72b29aa7a89c0e4c3a43f8167416378f0e
Erick Benitez <erick.benitez@avocadoblock.com>
2022-12-02 11:43:19 -0600

    update massive mint

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ["env"]
+            presets: ['env']
           }
         }
       }
