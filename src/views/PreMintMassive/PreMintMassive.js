10f9d740959d04d237aa77e0c50418f666a9391d
Erick Benitez <erick.benitez@avocadoblock.com>
2022-12-02 11:54:56 -0600

    update massive mint

--- src/views/PreMintMassive/PreMintMassive.js
+++ src/views/PreMintMassive/PreMintMassive.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import LoaderModal from '../../components/LoaderModal';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import {
@@ -13,10 +12,9 @@ import {
 const PreMintMassive = ({data}) => {
     const { t } = useTranslation("translate");
     const [msgLoad,setMsgLoad] = React.useState('Loading...');
-    const [load,setLoad] = React.useState(false);
+
    
     useEffect(() => {
-        setLoad(false);
         setMsgLoad(t("pre_mint_nft_massive.message_loader.confirm_pre_mint"))
         console.log(data);
     },[]);
@@ -28,17 +26,11 @@ const PreMintMassive = ({data}) => {
                     <CardContent>
                         <Box sx={{mb:'10px'}}>  
                             <ContentArea>
-                               MassiveMint
+                               MassiveMint {msgLoad}
                             </ContentArea>
                         </Box>
                     </CardContent>
             </Container>
-            <LoaderModal
-                text={msgLoad}
-                isOpen={load}
-                textColor='#fff'
-                spinnerColor='#fff'
-            />
         </Box>
     );
 };
