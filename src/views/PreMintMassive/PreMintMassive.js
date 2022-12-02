2e1838fca840ca302988d14a4653b39e6d937fad
Erick Benitez <erick.benitez@avocadoblock.com>
2022-12-02 12:02:06 -0600

    update

--- src/views/PreMintMassive/PreMintMassive.js
+++ src/views/PreMintMassive/PreMintMassive.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { useTranslation } from 'react-i18next';
-import PropTypes from 'prop-types';
 import {
     CardContent,
     ContentArea
@@ -10,14 +8,7 @@ import {
 
 
 const PreMintMassive = ({data}) => {
-    const { t } = useTranslation("translate");
-    const [msgLoad,setMsgLoad] = React.useState('Loading...');
 
-   
-    useEffect(() => {
-        setMsgLoad(t("pre_mint_nft_massive.message_loader.confirm_pre_mint"))
-        console.log(data);
-    },[]);
  
     return (
         <Box>
@@ -26,7 +17,7 @@ const PreMintMassive = ({data}) => {
                     <CardContent>
                         <Box sx={{mb:'10px'}}>  
                             <ContentArea>
-                               MassiveMint {msgLoad}
+                               MassiveMint {data.userAccount}
                             </ContentArea>
                         </Box>
                     </CardContent>
@@ -45,4 +36,4 @@ PreMintMassive.propTypes = {
 
 
 
-export default PreMintMassive;
+export {PreMintMassive};
